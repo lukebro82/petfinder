a47a3ab9cb01a3fffe5c0abd6464a51af0272c7e
Lucas <lucas.82@hotmail.com.ar>
2025-04-11 11:48:06 -0300

    cors *

--- be-src/index.ts
+++ be-src/index.ts
@@ -28,27 +28,15 @@ import { PetUpdateData } from "./models/pet";
 const app = express();
 const port = 3000;
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+// app.use(cors());
 
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://petfinder-react-front.onrender.com",
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true, // no hace daño tenerlo aunque no uses cookies
-//     methods: ["GET", "POST", "PATCH", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // crear usuario nuevo signup
 app.post("/auth", async (req, res) => {
